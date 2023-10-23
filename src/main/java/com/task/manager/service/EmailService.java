9df2081bfd4eedbdcd47d1f35189291583ad3a26
koskakos <fafsdfas93@mail.ru>
2023-10-23 21:16:08 +0600

    added @EnableAsync to EmailService.java

--- src/main/java/com/task/manager/service/EmailService.java
+++ src/main/java/com/task/manager/service/EmailService.java
@@ -3,9 +3,11 @@ package com.task.manager.service;
 import org.springframework.mail.SimpleMailMessage;
 import org.springframework.mail.javamail.JavaMailSender;
 import org.springframework.scheduling.annotation.Async;
+import org.springframework.scheduling.annotation.EnableAsync;
 import org.springframework.stereotype.Service;
 
 @Service
+@EnableAsync
 public class EmailService {
 
     private JavaMailSender javaMailSender;
